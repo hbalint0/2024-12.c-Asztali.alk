09b35bf0dfc5e87c08f4133413fea7b0cc5f0f9d
Bálint Horváth <2021130@dszcberegszaszi.hu>
2024-10-02 09:43:52 +0200

    pref

---
+++ shortform/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace shortform
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10,
+                y = 20;
+            Console.WriteLine($"Az x eredeti értéke: {x}");
+            Console.WriteLine($"Az x értke '++x ' iratással {++x}");
+            Console.WriteLine($"Az x értéke kiíratás után: {x}");
+            Console.WriteLine($"\nAz y eredeti értéke: {y}");
+            Console.WriteLine($"Az y értke 'y++ ' iratással {y++}");
+            Console.WriteLine($"Az y értéke kiíratás után: {y}");
+
+            Console.ReadKey();
+        }
+    }
+}

a51980f3f846c0f5962507b32a83a9c5ebf45eb6
Bálint Horváth <2021130@dszcberegszaszi.hu>
2024-10-02 08:52:38 +0200

    és kapcsolat

---
+++ valami/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace valami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool a = true,
+                b = true;
+            Console.WriteLine("Az és (&&) kapcsolat");
+
+            Console.WriteLine($"Ha a={a} és b={b}, akkor a&&b = {a && b}");
+
+            a = true;
+            b = false;
+
+            Console.WriteLine($"Ha a={a} és b={b}, akkor a&&b = {a && b}");
+
+            a = false;
+            b = false;
+
+            Console.WriteLine($"Ha a={a} és b={b}, akkor a&&b = {a && b}");
+
+            a = false;
+            b = true;
+
+            Console.WriteLine($"Ha a={a} és b={b}, akkor a&&b = {a && b}");
+
+               a = true;
+               b = true;
+            Console.WriteLine("\nAz és (&&) kapcsolat");
+
+            Console.WriteLine($"Ha a={a} és b={b}, akkor a&&b = {a && b}");
+
+            a = true;
+            b = false;
+
+            Console.WriteLine($"Ha a={a} és b={b}, akkor a&&b = {a && b}");
+
+            a = false;
+            b = false;
+
+            Console.WriteLine($"Ha a={a} és b={b}, akkor a&&b = {a && b}");
+
+            a = false;
+            b = true;
+
+            Console.WriteLine($"Ha a={a} és b={b}, akkor a&&b = {a && b}");
+            Console.ReadKey();
+        }
+    }
+}

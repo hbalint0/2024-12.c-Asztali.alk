2987680b5460acf2e6c0f75d06647e051877caa0
Bálint Horváth <2021130@dszcberegszaszi.hu>
2024-10-09 08:52:34 +0200

    10.09

---
+++ korhasab/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace korhasab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region 1. objektum
+            kor k1 = new kor();
+            k1.ReadSugar(15.0);
+            k1.SetKerulet();
+            k1.SetTerulet();
+            Console.WriteLine($"{k1.GetSugar()} sugarú kör kerülete {k1.GetKerulet()}, területe: {k1.GetTerulet()} ");
+            #endregion EOf 1. objektum
+            #region 2.objektum
+            kor k2 = new kor(28.7);
+            k2.SetKerulet();
+            k2.SetTerulet();
+            Console.WriteLine($"{k2.GetSugar()} sugarú kör kerülete {k2.GetKerulet()}, területe: {k2.GetTerulet()} ");
+            #endregion EOf 2. objektum
+            Console.ReadKey();
+        }
+    }
+}

48fe8f06d1b4d6e19664c990bd9de86ac256c7d2
2021130 <2021130@B104-06>
2025-02-05 09:34:15 +0100

    abstract

---
+++ abstract/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+     * Abstarctot mutatja be
+     * abstract osztály: csak száramztatott osztályon keresztül használható
+     * abstract metódus: csak abstract osztályon belül lehet és a származtatott osztályának kötelező megvalósítania
+     * 
+     */
+namespace @abstract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
